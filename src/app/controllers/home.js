900a91845e943a757526f60490dbc8cd32f87e92
Scott Prue <sprue.dev@gmail.com>
2016-04-10 20:14:37 -0400

    devshare added to load directory data for zipping.

--- src/app/controllers/home.js
+++ src/app/controllers/home.js
@@ -1,9 +1,53 @@
 'use strict'
-
+import Devshare from 'devshare'
+import Jszip from 'jszip'
+import filesave from 'node-safe-filesaver'
+import { each } from 'lodash'
+import Firepad from 'firepad'
 /**
  * New project page
  */
 
 export function index (req, res) {
-	res.json({message: 'Success'})
+  res.json({message: 'Success'})
+}
+
+export function zip (req, res) {
+  const fileSystem = Devshare
+      .project(req.params.owner, req.params.projectName)
+      .fileSystem
+
+  fileSystem
+    .get()
+    .then(directory => {
+      // console.log('directory loaded:', directory)
+      let zip = new Jszip()
+      let promiseArray = []
+      let handleZip = fbChildren => {
+        each(fbChildren, child => {
+          if (!child.meta || child.meta.entityType === 'folder') {
+            delete child.meta
+            return handleZip(child)
+          }
+          console.log('child', child)
+					if (child.original && !child.history) return zip.file(child.meta.path, child.original)
+          let promise = new Promise(resolve =>
+            Firepad.Headless(fileSystem.file(child.meta.path).firebaseRef()).getText(text => {
+              console.log('file text', text)
+							zip.file(child.meta.path, text || '')
+							resolve(text || '')
+            })
+          )
+          promiseArray.push(promise)
+        })
+      }
+      handleZip(directory)
+      return Promise.all(promiseArray).then(() => {
+        let content = zip.generate({ type: 'blob' })
+				console.log('promises fulfilled', `${req.params.projectname}-devShare-export.zip`)
+        let zipFile = filesave.saveAs(content, `${req.params.projectname}-devShare-export.zip`)
+        console.log('zip file created', zipFile)
+        res.send(zipFile)
+      })
+    })
 }
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,18 +1,18 @@
-'use strict';
+'use strict'
 
 /*!
  * Module dependencies.
  */
 
-import * as home from '../app/controllers/home';
-import config from './config';
+import * as home from '../app/controllers/home'
 
 /**
  * Expose routes
  */
 
 module.exports = function (app) {
-  app.get('/', home.index);
+  app.get('/', home.index)
+  app.get('/projects/:owner/:projectName/zip', home.zip)
 
   /**
    * Error handling
@@ -23,26 +23,26 @@ module.exports = function (app) {
     if (err.message
       && (~err.message.indexOf('not found')
       || (~err.message.indexOf('Cast to ObjectId failed')))) {
-      return next();
+      return next()
     }
 
-    console.error(err.stack);
+    console.error(err.stack)
 
     if (err.stack.includes('ValidationError')) {
-      res.status(422).render('422', { error: err.stack });
-      return;
+      res.status(422).render('422', { error: err.stack })
+      return
     }
 
     // error page
-    // res.status(500).render('500', { error: err.stack });
+    // res.status(500).render('500', { error: err.stack })
 
     // 500 Response
     res.status(500).json({
       message: 'Error.',
       stack: err.stack,
       code: 500
-    });
-  });
+    })
+  })
 
   // assume 404 since no middleware responded
   app.use((req, res) => {
@@ -50,6 +50,6 @@ module.exports = function (app) {
       message: 'Invalid request.',
       status: 'NOT_FOUND',
       code: 404
-    });
-  });
-};
+    })
+  })
+}
